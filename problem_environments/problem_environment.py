e3e9948ca81d65dbb6c9081bcd446443d1c87fdf
Beomjoon Kim <beomjoon@mit.edu>
2019-01-10 14:48:37 -0500

    3

--- problem_environments/problem_environment.py
+++ problem_environments/problem_environment.py
@@ -190,7 +190,8 @@ class ProblemEnvironment:
 
         if n_iterations is None:
             if self.name == 'convbelt':
-                n_iterations = [20, 50, 100, 500, 3000]
+                #n_iterations = [20, 50, 100, 500, 3000]
+                n_iterations = [20, 50, 100, 500, 1000]
             else:
                 n_iterations = [20, 50, 100, 500, 1000]
 
