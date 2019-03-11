806f0628aa7842c5edbf82a7a3fab04908986614
Beomjoon Kim <beomjoon@mit.edu>
2019-03-10 22:24:43 -0400

    3

--- generators/stounif_utils/stounif.py
+++ generators/stounif_utils/stounif.py
@@ -27,36 +27,6 @@ class StoUniform:
         self.widening_parameter = widening_parameter
         self.ucb_parameter = ucb_parameter
 
-    def is_ucb_step(self):
-        n_arms = len(self.arms)
-        if n_arms < 10:
-            return False
-        else:
-            if self.n_ucb_iterations < self.widening_parameter:
-                print "UCB iteration"
-                self.n_ucb_iterations += 1
-                return True
-            else:
-                self.n_ucb_iterations = 0
-                return False
-
-    def ucb_upperbound(self, arm):
-        ucb_value = arm.expected_value + self.ucb_parameter * np.sqrt(np.log(self.n_evaluations) / float(arm.n_visited))
-        return ucb_value
-
-    def perform_ucb(self):
-        best_value = -np.inf
-        best_arm = self.arms[0]
-        for arm in self.arms:
-            ucb_value = self.ucb_upperbound(arm)
-            if ucb_value > best_value:
-                best_arm = arm
-                best_value = ucb_value
-
-        import pdb;pdb.set_trace()
-
-        return best_arm
-
     def sample_next_point(self):
         dim_parameters = self.domain.shape[-1]
         domain_min = self.domain[0]
@@ -66,20 +36,15 @@ class StoUniform:
         return x
 
     def choose_next_point(self, dummy, dummy2):
-        if self.is_ucb_step():
-            print "Evaluating existing point using UCB"
-            x = self.perform_ucb()
-        else:
-            print "Evaluating a new point"
-            x = self.sample_next_point()
-            x = BanditArm(x)
+        print "Evaluating a new point"
+        x = self.sample_next_point()
+        x = BanditArm(x)
 
         return x
 
     def update_evaluated_arms(self, evaluated_arm, new_reward):
         self.n_evaluations += 1
         evaluated_arm.update_value(new_reward)
-        import pdb;pdb.set_trace()
         if not(evaluated_arm in self.arms):
             self.arms.append(evaluated_arm)
 
