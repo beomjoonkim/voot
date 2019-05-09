240334ce4b89a44d33577324d0f815b42da2fd43
Beomjoon Kim <beomjoon@mit.edu>
2019-05-09 10:26:33 -0400

    convbelt pw ready to be tested

--- test_scripts/test_mcts.py
+++ test_scripts/test_mcts.py
@@ -134,34 +134,39 @@ def main():
 
     if args.domain == 'convbelt':
         args.mcts_iter = 3000
-        args.voo_sampling_mode = 'uniform'
         args.n_switch = 10
         args.pick_switch = False
         args.use_max_backup = True
         args.n_feasibility_checks = 200
         args.problem_idx = 3
-        args.widening_parameter = 5
         args.n_actions_per_node = 3
-        if args.sampling_strategy == 'voo':
-            args.epsilon = 0.1
-        elif args.sampling_strategy == 'randomized_doo':
-            args.epsilon = 1.0
-    elif args.domain == 'minimum_displacement_removal':
-        args.mcts_iter = 2000
-        args.voo_sampling_mode = 'uniform'
-        args.n_switch = 10
-        args.pick_switch = True
-        args.use_max_backup = True
-        args.n_feasibility_checks = 50
-        args.problem_idx = 0
         if args.pw:
             args.sampling_strategy = 'unif'
             args.pw = True
             args.use_ucb = True
         else:
             args.widening_parameter = 5
-            args.n_actions_per_node = 1
             if args.sampling_strategy == 'voo':
+                args.voo_sampling_mode = 'uniform'
+                args.epsilon = 0.1
+            elif args.sampling_strategy == 'randomized_doo':
+                args.epsilon = 1.0
+    elif args.domain == 'minimum_displacement_removal':
+        args.mcts_iter = 2000
+        args.n_switch = 10
+        args.pick_switch = True
+        args.use_max_backup = True
+        args.n_feasibility_checks = 50
+        args.problem_idx = 0
+        args.n_actions_per_node = 1
+        if args.pw:
+            args.sampling_strategy = 'unif'
+            args.pw = True
+            args.use_ucb = True
+        else:
+            args.widening_parameter = 5
+            if args.sampling_strategy == 'voo':
+                args.voo_sampling_mode = 'uniform'
                 args.epsilon = 0.1
             elif args.sampling_strategy == 'randomized_doo':
                 args.epsilon = 1.0
