480b9a7ccf080f7394d0ef8c2150d5c102380fa2
beomjoon <beomjoon@mit.edu>
2019-09-05 09:49:57 -0400

    plotter update

--- plotters/plot_planning.py
+++ plotters/plot_planning.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import sys
 import glob
 
+import plot_rl
 
 def savefig(xlabel, ylabel, fname=''):
     plt.legend(loc='best', prop={'size': 13})
@@ -15,6 +16,7 @@ def savefig(xlabel, ylabel, fname=''):
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     print 'Saving figure ', fname + '.png'
+    plt.tight_layout()
     plt.savefig(fname + '.png', dpi=100, format='png')
 
 
@@ -37,6 +39,7 @@ def get_best_hyperparameter_dir(result_dir, problem_idx):
             file_problem_idx = int(fin.split('_')[-1].split('.')[0])
 
             if file_problem_idx != problem_idx:
+                print "file problem idx does not match the given problem idx"
                 continue
             try:
                 result = pickle.load(open(fdir + fin, 'r'))
@@ -76,8 +79,13 @@ def get_result_dir(algo_name, mcts_parameters):
     addendum = mcts_parameters.add
     n_switch = mcts_parameters.n_switch
 
+    if 'mdr' in mcts_parameters.domain:
+        domain = 'minimum_displacement_removal'
+    else:
+        domain = mcts_parameters.domain
+
     rootdir = './test_results/'
-    result_dir = rootdir + '/' + mcts_parameters.domain + '/mcts_iter_' + str(mcts_iter)
+    result_dir = rootdir + '/' + domain + '_results/mcts_iter_' + str(mcts_iter)
     if algo_name.find('pw') != -1:
         result_dir += '/pw_methods/uct_*_widening_*_unif'
     else:
@@ -100,8 +108,8 @@ def get_result_dir(algo_name, mcts_parameters):
 
     if mcts_parameters.domain.find('synthetic') != -1:
         if mcts_parameters.domain.find('rastrigin') != -1:
-            if mcts_parameters.problem_idx == 1 or algo_name.find('pw') !=-1:
-                result_dir += '_value_threshold_-50.0' #+ str(mcts_parameters.value_threshold)
+            if mcts_parameters.problem_idx == 1 or algo_name.find('pw') != -1:
+                result_dir += '_value_threshold_-50.0'  # + str(mcts_parameters.value_threshold)
         elif mcts_parameters.domain.find('shekel') != -1 and algo_name.find('pw') != -1:
             result_dir += '_value_threshold_' + str(mcts_parameters.value_threshold)
 
@@ -116,7 +124,7 @@ def get_result_dir(algo_name, mcts_parameters):
     problem_idx = mcts_parameters.problem_idx
 
     if algo_name == 'voo':
-        result_dir += '/sampling_mode/'+voo_sampling_mode+'/counter_ratio_1/eps_*/'
+        result_dir += '/sampling_mode/' + voo_sampling_mode + '/counter_ratio_1/eps_*/'
     elif algo_name.find('doo') != -1:
         result_dir += '/eps_*/'
     result_dir, best_rwd = get_best_hyperparameter_dir(result_dir, problem_idx)
@@ -141,13 +149,13 @@ def get_mcts_results(algo_name, mcts_parameters):
 
     for fin in list_dir:
         if fin.find('pidx') == -1:
-            #print "Continuing"
+            # print "Continuing"
             continue
         sd = int(fin.split('_')[2])
         file_problem_idx = int(fin.split('_')[-1].split('.')[0])
 
         if file_problem_idx != problem_idx:
-            #print "Continuing"
+            # print "Continuing"
             continue
         try:
             result = pickle.load(open(result_dir + fin, 'r'))
@@ -182,14 +190,14 @@ def get_mcts_results(algo_name, mcts_parameters):
 
 def get_max_rwds_wrt_time(search_rwd_times):
     max_time = 15000
-    organized_times = range(10, max_time, 1)
+    n_evals = range(10, max_time, 1)
 
     all_episode_data = []
     all_episode_progress_data = []
     for rwd_time in search_rwd_times:
-        episode_max_rwds_wrt_organized_times = []
-        episode_max_progress_wrt_organized_times = []
-        for organized_time in organized_times:
+        episode_max_rwds_wrt_n_evals = []
+        episode_max_progress_wrt_n_evals = []
+        for organized_time in n_evals:
             episode_times = np.array(rwd_time)[:, 0]
             episode_rwds = np.array(rwd_time)[:, 2]
             idxs = episode_times < organized_time
@@ -199,37 +207,35 @@ def get_max_rwds_wrt_time(search_rwd_times):
                 max_rwd = 0
             episode_progress = -np.array(rwd_time)[:, 3]
             # max_progress = np.max(episode_progress[idxs])
-            episode_max_rwds_wrt_organized_times.append(max_rwd)
-        # episode_max_progress_wrt_organized_times.append(max_progress)
-        all_episode_data.append(episode_max_rwds_wrt_organized_times)
-        all_episode_progress_data.append(episode_max_progress_wrt_organized_times)
+            episode_max_rwds_wrt_n_evals.append(max_rwd)
+        # episode_max_progress_wrt_n_evals.append(max_progress)
+        all_episode_data.append(episode_max_rwds_wrt_n_evals)
+        all_episode_progress_data.append(episode_max_progress_wrt_n_evals)
 
-    return np.array(all_episode_data), np.array(all_episode_progress_data), organized_times
+    return np.array(all_episode_data), np.array(all_episode_progress_data), n_evals
 
 
 def get_max_rwds_wrt_samples(search_rwd_times, n_evals):
-    organized_times = range(n_evals)
-
     all_episode_data = []
     all_episode_progress_data = []
     for rwd_time in search_rwd_times:
-        episode_max_rwds_wrt_organized_times = []
-        episode_max_progress_wrt_organized_times = []
-        for organized_time in organized_times:
+        episode_max_rwds_wrt_n_evals = []
+        episode_max_progress_wrt_n_evals = []
+        for organized_time in range(n_evals):
             episode_times = np.array(rwd_time)[:, 1]
             episode_rwds = np.array(rwd_time)[:, 2]
             episode_progress = -np.array(rwd_time)[:, 3]
             idxs = episode_times <= organized_time
             max_rwd = np.max(episode_rwds[idxs])
             max_progress = np.max(episode_progress[idxs])
-            episode_max_rwds_wrt_organized_times.append(max_rwd)
-            episode_max_progress_wrt_organized_times.append(max_progress)
-        all_episode_data.append(episode_max_rwds_wrt_organized_times)
-        all_episode_progress_data.append(episode_max_progress_wrt_organized_times)
-    return np.array(all_episode_data), np.array(all_episode_progress_data), organized_times
+            episode_max_rwds_wrt_n_evals.append(max_rwd)
+            episode_max_progress_wrt_n_evals.append(max_progress)
+        all_episode_data.append(episode_max_rwds_wrt_n_evals)
+        all_episode_progress_data.append(episode_max_progress_wrt_n_evals)
+    return np.array(all_episode_data), np.array(all_episode_progress_data)
 
 
-def get_algo_name(raw_name):
+def get_algo_name_to_put_on_plot(raw_name):
     if raw_name.find('randomized_doo') != -1:
         return "RandDOOT"
     elif raw_name.find('voo') != -1:
@@ -240,11 +246,15 @@ def get_algo_name(raw_name):
         return "PW-UCT"
     elif raw_name.find('doo') != -1:
         return "DOOT"
+    elif 'RL_ppo' in raw_name:
+        return "PPO"
+    elif 'RL_ddpg' in raw_name:
+        return "DDPG"
     else:
         raise ValueError
 
 
-def plot_across_algorithms():
+def setup_options():
     parser = argparse.ArgumentParser(description='MCTS parameters')
     parser.add_argument('-domain', type=str, default='minimum_displacement_removal_results')
     parser.add_argument('-w', type=float, default=5.0)
@@ -264,11 +274,8 @@ def plot_across_algorithms():
     parser.add_argument('-value_threshold', type=float, default=-40.0)
 
     args = parser.parse_args()
-    if args.domain.find('results') == -1:
-        args.domain +='_results'
-
-    if args.domain == 'convbelt_results':
-        domain_name = 'cbelt'
+    if args.domain == 'convbelt':
+        domain = 'cbelt'
         args.mcts_iter = 3000
         args.voo_sampling_mode = 'uniform'
         args.n_switch = 10
@@ -279,8 +286,8 @@ def plot_across_algorithms():
         args.w = 5.0
         args.n_actions_per_node = 3
         args.add = 'no_averaging'
-    elif args.domain == 'minimum_displacement_removal_results':
-        domain_name = 'mdr'
+    elif args.domain == 'mdr':
+        domain = 'mdr'
         args.mcts_iter = 2000
         args.voo_sampling_mode = 'uniform'
         args.n_switch = 10
@@ -292,7 +299,8 @@ def plot_across_algorithms():
         args.n_actions_per_node = 1
         args.add = 'no_averaging'
     elif args.domain.find('synthetic') != -1:
-        domain_name = args.domain
+        domain = args.domain
+        # note: I think for all objective functions, problem idx is 2?
         if args.problem_idx == 0:
             args.mcts_iter = 10000
             args.n_switch = 5
@@ -307,97 +315,116 @@ def plot_across_algorithms():
         args.voo_sampling_mode = 'centered_uniform'
         args.pick_switch = False
         args.use_max_backup = True
-        args.w = 100
+        args.w = 100  # note: I think for griewank widening should be 100
         args.n_actions_per_node = 1
     else:
         raise NotImplementedError
+    return args
 
-    if args.domain == 'convbelt_results':
-        algo_names = ['voo_uniform', 'pw', 'randomized_doo']
-    elif args.domain == 'minimum_displacement_removal_results':
-        algo_names = ['pw', 'voo_uniform', 'randomized_doo']
-    elif args.domain.find('synthetic') != -1:
-        if args.domain.find('shekel') != -1:
+
+def get_algo_names(options):
+    if options.domain == 'convbelt':
+        algo_names = ['RL_ppo', 'RL_ddpg', 'voo_uniform', 'pw', 'randomized_doo']
+    elif options.domain == 'mdr':
+        algo_names = ['RL_ppo', 'RL_ddpg', 'pw', 'voo_uniform', 'randomized_doo' ]
+    elif options.domain.find('synthetic') != -1:
+        if options.domain.find('shekel') != -1:
             algo_names = ['pw', 'voo_centered_uniform', 'doo']
-        elif args.domain.find('griewank') != -1:
+        elif options.domain.find('griewank') != -1:
             algo_names = ['pw', 'voo_centered_uniform', 'doo']
-        elif args.domain.find('rastrigin') != -1:
-            algo_names = ['voo_centered_uniform', 'doo']
+        elif options.domain.find('rastrigin') != -1:
             algo_names = ['pw', 'voo_centered_uniform', 'doo']
+    return algo_names
+
+
+def get_color_for_each_algorithm():
     color_dict = pickle.load(open('./plotters/color_dict.p', 'r'))
     color_names = color_dict.keys()
     color_dict[color_names[0]] = [0., 0.5570478679, 0.]
     color_dict['RandDOOT'] = [0, 0, 1]
-    color_dict['DOOT'] = [0, 0, 1]
     color_dict['VOOT'] = [1, 0, 0]
-    #color_dict['PW-UCT'] = np.array([0, 100, 0]) / 255.0
-    color_dict['PW-UCT'] = [0.8901960784313725, 0.6745098039215687, 0]
+    color_dict['PW-UCT'] = [0.8901960784313725, 0.6045098039215687, 0]
+    color_dict['PPO'] = [98/255., 37/255., 147/255.]
+    color_dict['DDPG'] = [15/255., 121/255., 147/255.]
+    return color_dict
 
-    max_rwds = []
+
+def get_color_for_algo(algo_name, color_dict):
+    if algo_name in color_dict.keys():
+        color = color_dict[algo_name]
+    else:
+        color = np.random.random((1, 3))
+
+    return color
+
+
+def get_plot_name(options):
+    if options.p:
+        plot_name = 'progress_toy_' + options.domain + '_problem_idx_' + str(options.problem_idx) + '_w_' + str(
+            options.w) + '_mcts_iter_' + str(options.mcts_iter) \
+                    + "_uct_" + str(options.uct) + "_n_feasibility_checks_" + str(
+            options.n_feasibility_checks) + "_pw_" + str(options.pw)
+        plot_name = options.domain+ '_progress'
+    else:
+        plot_name = 'reward_toy_' + options.domain + '_problem_idx_' + str(options.problem_idx) + '_w_' + str(
+            options.w) \
+                    + '_mcts_iter_' \
+                    + str(options.mcts_iter) + "_uct_" + str(options.uct) + "_n_feasibility_checks_" \
+                    + str(options.n_feasibility_checks) + '_use_max_backup_' + str(options.use_max_backup) \
+                    + '_pick_switch_' + str(options.pick_switch) + "_pw_" + str(options.pw)
+
+        if options.domain.find('synthetic') != -1:
+            plot_name += '_value_threshold_' + str(options.value_threshold)
+
+        if options.domain == 'convbelt_results':
+            plot_name += '_n_actions_per_node_' + str(options.n_actions_per_node)
+
+        if options.n_switch != -1:
+            plot_name += "_n_switch_" + str(options.n_switch)
+        plot_name = options.domain+ '_rewards'
+    return plot_name
+
+
+def plot_optimums(options):
+    n_evals = options.mcts_iter
+    if options.domain == 'mdr':
+        if options.p:
+            sns.tsplot([0] * n_evals, range(options.mcts_iter),
+                       ci=95, condition='No objects remaining', color='magenta')
+        else:
+            sns.tsplot([4.1] * n_evals, range(options.mcts_iter), ci=95,
+                       condition='Avg feasible reward', color='magenta')
+
+
+def plot_across_algorithms():
+    options = setup_options()
+    algo_names = get_algo_names(options)
+    color_dict = get_color_for_each_algorithm()
+
     for algo_idx, algo in enumerate(algo_names):
         print algo
-        try:
-            search_rwd_times, max_rwd = get_mcts_results(algo, args)
-        except OSError:
-            print algo, "not found"
-            continue
-        search_rwd, search_progress, organized_times = get_max_rwds_wrt_samples(search_rwd_times, args.mcts_iter)
 
-        max_rwds.append(max_rwd)
-        algo_name = get_algo_name(algo)
-
-        if algo_name in color_dict.keys():
-            color = color_dict[algo_name]
+        algo_name = get_algo_name_to_put_on_plot(algo)
+        if 'PPO' in algo_name or 'DDPG' in algo_name:
+            fdir = 'RL_results/%s/n_data_100/%s/dg_lr_0.001_0.0001/' % (options.domain, algo_name.lower())
+            search_rwd, search_progress = plot_rl.get_max_rwds_wrt_samples(fdir)
         else:
-            color = np.random.random((1, 3))
+            search_rwd_times, max_rwd = get_mcts_results(algo, options)
+            search_rwd, search_progress = get_max_rwds_wrt_samples(search_rwd_times, options.mcts_iter)
+        color = get_color_for_algo(algo_name, color_dict)
 
-        if args.p:
-            sns.tsplot(search_progress[:, :args.mcts_iter], organized_times[:args.mcts_iter], ci=95,
-                       condition=algo_name,
-                       color=color)
-        else:
-            try:
-                sns.tsplot(search_rwd[:, :], organized_times[:], ci=95, condition=algo_name,
-                           color=color)
-            except:
-                continue
+        content_to_plot = search_progress if options.p else search_rwd
+        sns.tsplot(content_to_plot[:, :options.mcts_iter], ci=95, condition=algo_name, color=color)
 
-    if args.p:
-        plot_name = 'progress_toy_' + domain_name + '_problem_idx_' + str(args.problem_idx) + '_w_' + str(
-            args.w) + '_mcts_iter_' + str(args.mcts_iter) \
-                    + "_uct_" + str(args.uct) + "_n_feasibility_checks_" + str(
-            args.n_feasibility_checks) + "_pw_" + str(args.pw)
-        if args.domain != 'convbelt_results':
-            sns.tsplot([0] * len(organized_times[:]), organized_times[:args.mcts_iter],
-                       ci=95, condition='Avg feasible reward', color='magenta')
-    else:
-        if domain_name == 'mdr':
-            sns.tsplot([4.1] * len(organized_times[:]), organized_times[:args.mcts_iter],
-                       ci=95, condition='Avg feasible reward', color='magenta')
+    plot_name = get_plot_name(options)
+    plot_optimums(options)
 
-        plot_name = 'reward_toy_' + domain_name + '_problem_idx_' + str(args.problem_idx) + '_w_' + str(args.w) \
-                    + '_mcts_iter_' \
-                    + str(args.mcts_iter) + "_uct_" + str(args.uct) + "_n_feasibility_checks_" \
-                    + str(args.n_feasibility_checks) + '_use_max_backup_' + str(args.use_max_backup) \
-                    + '_pick_switch_' + str(args.pick_switch) + "_pw_" + str(args.pw)
-
-        if domain_name.find('synthetic') != -1:
-            plot_name += '_value_threshold_' + str(args.value_threshold)
-
-        if args.domain == 'convbelt_results':
-            plot_name += '_n_actions_per_node_' + str(args.n_actions_per_node)
-
-        if args.n_switch != -1:
-            plot_name += "_n_switch_" + str(args.n_switch)
-
-    if domain_name == 'cbelt':
-        if not args.p:
+    if options.domain == 'cbelt':
+        if not options.p:
             plt.ylim(-2, 4.5)
-    if args.p:
-        savefig('Number of simulations', 'Number of remaining objects',
-                fname='./plotters/' + args.add + '_toy_' + plot_name)
-    else:
-        savefig('Number of simulations', 'Average rewards', fname='./plotters/' + args.add + '_toy_' + plot_name)
+    y_axis_label = 'Number of remaining objects' if options.p else 'Average rewards'
+
+    savefig('Number of simulations', y_axis_label, fname='./plotters/' + options.add + plot_name)
 
 
 if __name__ == '__main__':
--- plotters/plot_rl.py
+++ plotters/plot_rl.py
@@ -4,6 +4,7 @@ import os
 import csv
 import numpy as np
 
+
 def savefig(xlabel, ylabel, fname=''):
     plt.legend(loc='best', prop={'size': 13})
     plt.xlabel(xlabel, fontsize=14, fontweight='bold')
@@ -14,35 +15,55 @@ def savefig(xlabel, ylabel, fname=''):
     plt.savefig(fname + '.png', dpi=100, format='png')
 
 
-def get_results_across_seeds(fdir):
-    explr_dirs = os.listdir(fdir)
-    for explr_dir in explr_dirs:
-        file_dir = fdir+'/'+explr_dir+'/n_score_5/n_trial_-1/train_results'
-        max_sumR_over_seeds = []
+def get_max_rwds_wrt_samples(fdir):
+    tau_dirs = os.listdir(fdir)
+    for tau_dir in tau_dirs:
+        file_dir = fdir + '/' + tau_dir + '/explr_p_0.1/n_score_5/n_trial_-1/train_results'
+        max_rwds_over_seeds = []
+        max_progress_over_seeds = []
         for result_file in os.listdir(file_dir):
-            max_sumRs = []
-            max_sumR = -np.inf
+            max_rwds = []
+            max_progresses = []
+            max_episode_rwd = -np.inf
+            max_episode_progress = -np.inf
             if 'performance.txt' in result_file:
-                fopen = open(file_dir+'/'+result_file, 'r')
+                fopen = open(file_dir + '/' + result_file, 'r')
                 rdr = csv.reader(fopen, delimiter=',')
                 for row in rdr:
-                    episode_sumR = float(row[1])
-                    if episode_sumR > max_sumR:
-                        max_sumR = episode_sumR
-                    max_sumRs.append(max_sumR)
-                max_sumR_over_seeds.append(max_sumRs)
-    return np.array(max_sumR_over_seeds)
+                    episode_rwd = float(row[1])
+                    episode_progress = -float(row[2])
+
+                    if episode_rwd > max_episode_rwd:
+                        max_episode_rwd = episode_rwd
+                    if episode_progress > max_episode_progress:
+                        max_episode_progress = episode_progress
+
+                    max_rwds.append(max_episode_rwd)
+                    max_progresses.append(max_episode_progress)
+
+                max_rwds_over_seeds.append(max_rwds)
+                max_progress_over_seeds.append(max_progresses)
+    return np.array(max_rwds_over_seeds), np.array(max_progress_over_seeds)
 
 
-def plot_across_algorithms():
+def plot_over_different_taus():
     algo_dir = 'RL_results/mdr/n_data_100/ddpg/dg_lr_0.001_0.0001/'
     taudirs = os.listdir(algo_dir)
 
     for taudir in taudirs:
-        results = get_results_across_seeds(algo_dir+taudir)
-        sns.tsplot(results, condition=taudir,ci=95,color=np.random.rand(3))
+        results = get_results_across_seeds(algo_dir + taudir, False)
+        sns.tsplot(results, condition=taudir, ci=95, color=np.random.rand(3))
     savefig('X', 'Rwds')
+
+    # best taus:
+    #   convbelt, ddpg - 1e-4
+    #   mdr, ddpg - 1e-3
+    #   mdr, ppo - 0.1
+    #   convbelt, ppo - 0.3
+
+def main():
+    plot_over_different_taus()
 
 
 if __name__ == '__main__':
-    plot_across_algorithms()
+    main()
